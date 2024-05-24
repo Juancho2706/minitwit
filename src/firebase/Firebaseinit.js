158df8d842cc7fccaa4be6ce38411af53469bc05
Juan Villegas <jvillegas.dev@gmail.com>
2024-05-23 22:25:23 -0400

    Bases

---
+++ src/firebase/Firebaseinit.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBymCeWyu4tA_25IE5CQiM0WkfjlA9Ubl0",
+  authDomain: "minitwit-24677.firebaseapp.com",
+  projectId: "minitwit-24677",
+  storageBucket: "minitwit-24677.appspot.com",
+  messagingSenderId: "449656356703",
+  appId: "1:449656356703:web:bbd82d2a3268b04cc4c0ea",
+  measurementId: "G-XGCJJZWYPR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const autorizacion = getAuth(app)
+export const googleprovider = new GoogleAuthProvider()
